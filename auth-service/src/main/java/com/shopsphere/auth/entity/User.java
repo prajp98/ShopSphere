10ae9189f3c32ed6c44bff20f9d259d101d54d39
Prajwala <praj.p98@gmail.com>
2025-07-22 20:04:20 +0530

    Auth entity & repo

---
+++ auth-service/src/main/java/com/shopsphere/auth/entity/User.java
@@ -0,0 +1,33 @@
+package com.shopsphere.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String role; // USER, ADMIN
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
