ca893ba124b1068c60c2fb4c712d2a184e146993
prajwalaparameshwar <praj.p98@gmail.com>
2025-08-01 11:56:55 +0530

    Version change

--- product-service/src/main/java/com/shopsphere/product/ProductServiceApplication.java
+++ product-service/src/main/java/com/shopsphere/product/ProductServiceApplication.java
@@ -1,11 +1,21 @@
 package com.shopsphere.product;
 
+import com.shopsphere.product.security.JwtAuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProductServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
+    }
+    @Bean
+    public FilterRegistrationBean<JwtAuthFilter> jwtFilter(JwtAuthFilter filter) {
+        FilterRegistrationBean<JwtAuthFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(filter);
+        registrationBean.addUrlPatterns("/products/*"); // Protect all product APIs
+        return registrationBean;
     }
 }
